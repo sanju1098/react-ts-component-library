055f12e9c78e8688d9ca9b380dc5cfaeeca80ce4
Sanjay Kumar S R <49760746+sanju1098@users.noreply.github.com>
2025-01-11 09:03:16 +0530

    Update Button.tsx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,7 +5,7 @@ import { ButtonProps } from './Button.types';
 
 const Button: FC<ButtonProps>  = ({ type, text, onClick }) => {
     return (
-        <button type="button" className={`Button Button-${type}`} onClick={onClick}>
+        <button type="button" className={`button button-${type}`} onClick={onClick}>
             {text}
         </button>
     )
